450f1f426966340c7e181db39514589269342b83
asplogic <hon808@gmail.com>
2018-10-30 13:57:25 -0700

    Updates

--- cassandra/build-and-push.sh
+++ cassandra/build-and-push.sh
@@ -2,7 +2,7 @@
 # You must be logged in
 
 docker build -t build/cassandra .
-docker tag build/cassandra classandra:3.11.3
+docker tag build/cassandra gcr.io/pso-examples/cassandra:3.11.3-v22
 docker push quay.io/asplogic/cassandra:3.11.3
 docker tag build/cassandra quay.io/asplogic/cassandra:latest
 docker push quay.io/asplogic/cassandra:latest
